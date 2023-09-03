92a236054ceff740fd1ca04d9c4ac4eebb1d9504
vaishnavwalke <84281792+vaishnavwalke@users.noreply.github.com>
2023-09-03 13:03:43 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import HeroSection from "./components/Hero";
+import "./components/Navigation";
+import Navigation from "./components/Navigation";
+const App = () => {
+  return (
+    <div>
+      <Navigation />
+      <HeroSection />
+    </div>
+  );
+};
+
+export default App;
